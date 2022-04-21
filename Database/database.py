23c95e89844e13aeeedfc6f1c6c45d3dd1d8b536
Eduardo <lovato.py@gmail.com>
2022-04-21 00:38:20 -0300

    Database adicionado
    Database adicionado com o intuito de facilitar a execução do script do banco

---
+++ Database/database.py
@@ -0,0 +1,26 @@
+import mysql.connector
+conexao = mysql.connector.connect(host="localhost",user="root",password="")
+
+cursor = conexao.cursor()
+
+try:
+    cursor.execute("CREATE DATABASE db_user;")
+    # UTILIZA O BANCO DE DADOS
+    cursor.execute("USE db_user;")
+    # CRIA A A TABELA USUARIO
+    cursor.execute("CREATE TABLE tb_user(\ntb_user_id integer not null auto_increment primary key,\ntb_user_nome varchar(100) not null,\n tb_user_email varchar(100) not null,\ntb_user_pass varchar(100)not null,\ntb_user_sexo char(1)not null,\ntb_user_permission BOOLEAN\n);")
+
+    cursor.execute("INSERT INTO `db_user`.`tb_user` (`tb_user_nome`, `tb_user_email`, `tb_user_pass`, `tb_user_sexo`, `tb_user_permission`) VALUES ('Eduardo', 'lovato.py@gmail.com', '12345678', 'M', '1');")
+    cursor.execute("INSERT INTO `db_user`.`tb_user` (`tb_user_nome`, `tb_user_email`, `tb_user_pass`, `tb_user_sexo`, `tb_user_permission`) VALUES ('Fernando', 'fernando@yahoo.com', '123456', 'M', '0');")
+    cursor.execute("INSERT INTO `db_user`.`tb_user` (`tb_user_nome`, `tb_user_email`, `tb_user_pass`, `tb_user_sexo`, `tb_user_permission`) VALUES ('Allan', 'allan@outlook.com', '419256', 'M', '0');")
+    cursor.execute("INSERT INTO `db_user`.`tb_user` (`tb_user_nome`, `tb_user_email`, `tb_user_pass`, `tb_user_sexo`, `tb_user_permission`) VALUES ('Ester', 'ester@aws.com', '400289', 'F', '0');")
+    cursor.execute("INSERT INTO `db_user`.`tb_user` (`tb_user_nome`, `tb_user_email`, `tb_user_pass`, `tb_user_sexo`, `tb_user_permission`) VALUES ('Pedro', 'pedro@azure.com', '456548', 'M', '0');")
+    cursor.execute("INSERT INTO `db_user`.`tb_user` (`tb_user_nome`, `tb_user_email`, `tb_user_pass`, `tb_user_sexo`, `tb_user_permission`) VALUES ('Marcelo', 'marcelo@gmail.com', '511878', 'M', '0');")
+    cursor.execute("INSERT INTO `db_user`.`tb_user` (`tb_user_nome`, `tb_user_email`, `tb_user_pass`, `tb_user_sexo`, `tb_user_permission`) VALUES ('Alvaro', 'alvaro@gmail.com', '465454', 'M', '0');")
+    cursor.execute("INSERT INTO `db_user`.`tb_user` (`tb_user_nome`, `tb_user_email`, `tb_user_pass`, `tb_user_sexo`, `tb_user_permission`) VALUES ('Marcela', 'marcela@youtube.com', '456565', 'F', '0');")
+    cursor.execute("INSERT INTO `db_user`.`tb_user` (`tb_user_nome`, `tb_user_email`, `tb_user_pass`, `tb_user_sexo`, `tb_user_permission`) VALUES ('Gabriela', 'gabi@globo.com', '456546', 'F', '0');")
+    cursor.execute("INSERT INTO `db_user`.`tb_user` (`tb_user_nome`, `tb_user_email`, `tb_user_pass`, `tb_user_sexo`, `tb_user_permission`) VALUES ('Estela', 'estela@accenture.com', '465646', 'F', '0');")
+    cursor.execute("INSERT INTO `db_user`.`tb_user` (`tb_user_nome`, `tb_user_email`, `tb_user_pass`, `tb_user_sexo`, `tb_user_permission`) VALUES ('Maria', 'maria@zoom.com', '545545', 'F', '0');")
+    conexao.commit()
+except:
+    print('ERRO')
